834947510ed015c0e00b4b9eb297e1c36d89ffef
natasyaabygaill <ntsyaabygaill@gmail.com>
2020-05-16 20:18:29 +0700

    vers 1

---
+++ application/views/common/header.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html lang="zxx">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>E-review</title>
+	<link href="<?php echo base_url('img/favicon.png') ?>" rel="icon">
+	<!-- Bootstrap CSS -->
+	<link href="<?php echo base_url('css/bootstrap.min.css') ?>" rel="stylesheet">
+	<!-- animate CSS -->
+	<link href="<?php echo base_url('css/animate.css') ?>" rel="stylesheet">
+	<!-- owl carousel CSS -->
+	<link href="<?php echo base_url('css/owl.carousel.min.css') ?>" rel="stylesheet">
+	<!-- font awesome CSS -->
+	<link href="<?php echo base_url('css/all.css') ?>" rel="stylesheet">
+	<!-- flaticon CSS -->
+	<link href="<?php echo base_url('css/flaticon.css') ?>" rel="stylesheet">
+	<link href="<?php echo base_url('css/themify-icons.css') ?>" rel="stylesheet">
+	<link href="<?php echo base_url('css/swiper.min.css') ?>" rel="stylesheet">
+	<!-- font awesome CSS -->
+	<link href="<?php echo base_url('css/magnific-popup.css') ?>" rel="stylesheet">
+	<!-- swiper CSS -->
+	<link href="<?php echo base_url('css/slick.css') ?>" rel="stylesheet">
+	<!-- style CSS -->
+	<link href="<?php echo base_url('css/style.css') ?>" rel="stylesheet">
+    
+</head>
+<!--::header part start::-->
+<header class="main_menu home_menu">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <a class="navbar-brand" href="index.html"> <img src="img/logo.png" alt="logo"> </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="menu_icon"><i class="fas fa-bars"></i></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse main-menu-item" id="navbarSupportedContent">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="index.html">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="about.html">about</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="services.html">services</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="" id="navbarDropdownpro"
+                                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        portfolio
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdownpro">
+                                        <a class="dropdown-item" href="portfolio.html">portfolio</a>
+                                        <a class="dropdown-item" href="portfolio_details.html">portfolio details</a>
+                                    </div>
+                                </li>
+
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown"
+                                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Blog
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="blog.html"> blog</a>
+                                        <a class="dropdown-item" href="single-blog.html">Single blog</a>
+                                    </div>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown1"
+                                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        pages
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
+                                        <a class="dropdown-item" href="elements.html">Elements</a>
+                                    </div>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="contact.html">contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="dropdown cart">
+                            <a class="dropdown-toggle" href="#" id="navbarDropdown3" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="flaticon-bag"></i>
+                            </a>
+                            <!-- <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <div class="single_product">
+    
+                                    </div>
+                                </div> -->
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header part end-->
